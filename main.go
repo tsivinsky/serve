3a4e6ea64b6c9e9ec39b2ea595f88be30afde8f4
Daniil Tsivinsky <daniil@tsivinsky.com>
2025-03-21 17:58:29 +0300

    add `-cert` & `-key` files for https server

--- main.go
+++ main.go
@@ -10,22 +10,36 @@ var (
 	dir  = flag.String("S", ".", "directory to serve")
 	host = flag.String("H", "localhost", "server host")
 	port = flag.Int("p", 5000, "server port")
+
+	certFile    = flag.String("cert", "", "ssl certificate file")
+	certKeyFile = flag.String("key", "", "ssl certificate key file")
 )
 
 func getServerAddr(host string, port int) string {
 	return fmt.Sprintf("%s:%d", host, port)
 }
 
-func handler(addr string, dir string) http.Handler {
-	fmt.Printf("Listening on http://%s\n", addr)
+func handler(addr string, dir string, https bool) http.Handler {
+	protocol := "http:"
+	if https {
+		protocol = "https:"
+	}
+
+	fmt.Printf("Listening on %s//%s\n", protocol, addr)
 	return http.FileServer(http.Dir(dir))
 }
 
 func main() {
 	flag.Parse()
 
+	var err error
 	addr := getServerAddr(*host, *port)
-	if err := http.ListenAndServe(addr, handler(addr, *dir)); err != nil {
+	if *certFile != "" && *certKeyFile != "" {
+		err = http.ListenAndServeTLS(addr, *certFile, *certKeyFile, handler(addr, *dir, true))
+	} else {
+		err = http.ListenAndServe(addr, handler(addr, *dir, false))
+	}
+	if err != nil {
 		panic(err)
 	}
 }
--- readme.md
+++ readme.md
@@ -5,3 +5,9 @@
 ```bash
 serve
 ```
+
+### https server
+
+```bash
+serve -cert file.crt -key file.key
+```
